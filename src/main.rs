a281087cddb4b9de1d4bea00794182c852e0b7d7
Abhishek Bhardwaj <abshkbh@gmail.com>
2019-03-16 10:05:54 +0000

    Process in built commands

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use nix::sys::signal::*;
 use nix::sys::wait::*;
 use nix::unistd::*;
 use std::fmt;
-use std::sync::atomic::AtomicBool;
 use std::io::{self, Read};
+use std::process;
+use std::sync::atomic::AtomicBool;
 
 pub enum InBuiltCmd {
     Quit,
     Jobs,
     Bg(i32),
-    Fg(i33),
+    Fg(i32),
     Kill(i32),
 }
 
@@ -51,14 +52,56 @@ pub struct Shell {
     jobs: Vec<Job>,
 }
 
+impl Shell {
+    pub fn maybe_run_in_built_cmd(&self, cmd: &String) -> bool {
+        match cmd.as_ref() {
+            "quit" => {
+                println!("quit");
+                process::exit(0);
+                true
+            }
+            "jobs" => {
+                println!("jobs");
+                self.print_jobs();
+                true
+            }
+            _ => {
+                println!("Need to run {}", cmd);
+                false
+            }
+        }
+    }
+
+    fn print_jobs(&self) {
+        println!("Jobs");
+        for job in &self.jobs {
+            println!("{}", job)
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     println!("Welcome to my shell");
+    let shell = Shell {
+        fg: None,
+        jobs: vec![],
+    };
 
     loop {
+        // Parse command.
         let mut command = String::new();
         match io::stdin().read_line(&mut command) {
-            Ok(_) => println!("Command: {}", command),
+            Ok(_) => {
+                // Remove trailing characters.
+                command = command.trim().to_string();
+            }
             Err(error) => println!("Error reading command"),
+        }
+
+        // Process the command.
+        if (!shell.maybe_run_in_built_cmd(&command)) {
+            // TODO: Fork and exec here.
+            println!("Run process")
         }
     }
     Ok(())
